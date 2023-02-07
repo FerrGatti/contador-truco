02e810e249723289e957ddeab0f397354cab60b5
Fernando Gatti <fernandogatti74@gmail.com>
2023-02-07 17:39:36 -0300

    Update index.js

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,12 +1,9 @@
 import {Contador} from "./contador.js"
 
 const modal = document.querySelector('dialog');
-const partidaGuardada = JSON.parse(localStorage.getItem("truco"));
-const nosotrosNombre = "Nosotros";
-const ellosNombre = "Ellos";
 
-const nosotros = new Contador(nosotrosNombre, document.getElementById("nosotros"), partidaGuardada[nosotrosNombre]);
-const ellos = new Contador(ellosNombre, document.getElementById("ellos"), partidaGuardada[ellosNombre]);
+const nosotros = new Contador("Nosotros", document.getElementById("nosotros"));
+const ellos = new Contador("Ellos", document.getElementById("ellos"));
 
 document.getElementById("reset").addEventListener("click", ()=> modal.showModal());
 document.getElementById("volver").addEventListener("click", ()=> modal.close());
@@ -15,15 +12,3 @@ document.getElementById("aceptar").addEventListener("click", ()=> {
     ellos.reset();
     modal.close();
 });
-
-document.querySelectorAll("button").forEach(button =>{
-    button.addEventListener("click", ()=>{
-        const partidaAguardar = {
-            [nosotros.nombre] : nosotros.numero,
-            [ellos.nombre] : ellos.numero,
-        }
-
-        localStorage.setItem("truco",JSON.stringify(partidaAguardar));
-        console.log("Guardado");
-    })
-})
